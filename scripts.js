a37c59bf2a40f26ca2e7364da65541549acaf017
T1ag0o <arnhold.tiagoR@outlook.com>
2026-05-01 11:04:42 -0400

    ajustando o contador e a somatória

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const category = document.getElementById("category");
 
 // resgatando a lista de despesas
 const expenseList = document.querySelector("ul");
+const expensesTotal = document.querySelector("aside header h2");
 
 // formatação do valor do input amount para aceitar apenas números
 amount.oninput = () => {
@@ -45,6 +46,8 @@ form.onsubmit = (event) => {
   expenseAdd(newExpense);
 };
 
+
+// função para adicionar um novo gasto na lista
 function expenseAdd(newExpense) {
   try {
     
@@ -74,18 +77,75 @@ function expenseAdd(newExpense) {
     expenseAmount.setAttribute("class", "expense-amount");
     expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`;
 
+    // cria o botão de excluir
+    const deleteButton = document.createElement("img");
+    deleteButton.setAttribute("src", "img/remove.svg");
+    deleteButton.setAttribute("alt", "Excluir despesa");
+    deleteButton.classList.add("remove-icon");
+
 
     // adiciona nome e categoria no container
     expenseInfo.append(expenseName, expenseCategory);
 
     // adiciona as informações no item
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, deleteButton);
 
     // adiciona o item na lista
     expenseList.append(expenseItem);
+
+    updateTotals();
 
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
   };
+};
+
+// Atualiza as quantidades totais
+function updateTotals() {
+  try {
+    // recupera os (li) da (ul)
+    const items = expenseList.children;
+
+    // recupera o contador de despesas
+    const expenseCount = document.getElementById("qtd-expenses");
+    expenseCount.textContent = `${items.length} ${items.length > 1 ? 'despesas' : 'despesa'}`;
+
+    // atualiza o valor de total despesas
+    let total = 0;
+
+    for(let item = 0; item < items.length; item++) {
+      const itemAmount = items[item].querySelector(".expense-amount");
+
+      // remove os caracteres não numericos
+      let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",", ".");
+
+      value = parseFloat(value);
+
+      // verifica se é um número válido
+      if (isNaN(value)) {
+        return alert("Não foi possível calcular o total");
+      };
+
+      // incremente o total
+      total += Number(value);
+
+    };
+  
+    // cria span para adicionar o 'R$'
+    const symbolBRL = document.createElement("small");
+    symbolBRL.textContent = "R$";
+
+    total = formatCurrencyBRL(total).toUpperCase().replace("R$", "");
+
+    // limpa o conteudo
+    expensesTotal.innerHTML = "";
+
+    // adiciona o simbolo da moeda e o valor formatado
+    expensesTotal.append(symbolBRL, total);
+
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível atualizar os totais");
+  }
 };
